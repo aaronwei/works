ea844772ce2c6f5c639b3bc3aa462abdd7f25c5f
aaronwei <weijunqi0000@126.com>
2015-02-10 14:10:45 +0800

    那个女孩

---
+++ myapp/src/com/example/myapp/Listener/GoBackOnClickListener.java
@@ -0,0 +1,27 @@
+package com.example.myapp.Listener;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.View;
+import com.example.myapp.MyActivity;
+import com.example.myapp.R;
+import com.example.myapp.TowActivity;
+
+/**
+ * Created by aaron on 2015/2/10.
+ */
+public class GoBackOnClickListener implements View.OnClickListener {
+    private Activity activity;
+
+    public GoBackOnClickListener(Activity activity){
+        this.activity = activity;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this.activity, MyActivity.class);
+        activity.startActivityForResult(intent, 0);
+        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_rigth_out);
+    }
+}
